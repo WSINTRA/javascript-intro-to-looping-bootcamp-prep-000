f8691a933f80a49e0cd4b30ce90ccd41913063ce
William Robert Simpson <will.simpson85@gmail.com>
2018-11-20 17:30:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 var ar = [];
 function forLoop(a){
   for (let i = 0; i < 26; i++) {
-  output = `I am ${i} strange loop${i === 1 ? '' : 's'}. `
+  output = `I am ${i} strange loop${i === 1 ? '' : 's'}.`
 
   a[a.length] = output
     }
